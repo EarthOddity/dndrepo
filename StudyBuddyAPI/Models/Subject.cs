a04c43a9d07c3e89537586d2cc78fc8973f470cf
M316354 <316354@viauc.dk>
2024-10-28 16:11:19 +0100

    design of the subject class

---
+++ StudyBuddyAPI/Models/Subject.cs
@@ -0,0 +1,15 @@
+public class Subject
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Bachelor[] Bachelors { get; set; }
+
+    public Subject(int id, string name, string description, Bachelor[] bachelors)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Bachelors = bachelors;
+    }
+}
