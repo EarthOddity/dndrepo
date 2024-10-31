38b94c85de8c5055b6cd2e0ff45b9e452bbe2c7a
EarthOddity <316353@via.dk>
2024-10-31 17:48:20 +0100

    solving issues with files

---
+++ StudyBuddyAPI/Services/ModeratorService.cs
@@ -0,0 +1,50 @@
+namespace StudyBuddyAPI.models;
+
+public class ModeratorService{
+    private static List<Moderator> moderatorsList = new List<Moderator>();
+    private static List<Review> reviews = new List<Review>();
+
+
+    static ModeratorService(){
+    }
+      public Task<IEnumerable<Moderator>> GetAllModerators()
+    {
+        return Task.FromResult(moderatorsList.AsEnumerable());
+    }
+
+    public Task AddModerator(Moderator moderator)
+    {
+        moderatorsList.Add(moderator);
+        return Task.CompletedTask;
+    }
+
+    /*public Task BanReview(int id, Review review){
+        var moderator = moderatorsList.FirstOrDefault(m => m.id == id);
+        var _review = reviews.FirstOrDefault(r => r.Id == reviewId);
+
+        if(moderator != null){
+            moderator.bannedReviews.Add(review);
+            reviews.Remove(_review); // We will have to remove it from the actual database here with a Delete method from the review service 
+        }
+        return Task.CompletedTask;
+    }*/
+
+    public Task AssignSection(int id, string section){
+        var moderator = moderatorsList.FirstOrDefault(m => m.id == id);
+        if(moderator != null && !moderator.assignedSections.Contains(section)){
+            moderator.assignedSections.Add(section);
+        }
+        return Task.CompletedTask;
+    }
+
+     public Task<Moderator> GetModeratorById(int id){
+        var moderator = moderatorsList.FirstOrDefault(m => m.id == id);
+        return Task.FromResult(moderator);
+     }
+
+     public Task deleteModerator(int id){
+        var moderator = moderatorsList.FirstOrDefault(m => m.id == id);
+        moderatorsList.Remove(moderator);
+        return Task.CompletedTask;
+     }
+}
