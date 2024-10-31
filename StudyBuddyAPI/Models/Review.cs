e943512d0855825b3733af9960b70871d5a660ab
EarthOddity <316353@via.dk>
2024-10-31 19:40:52 +0100

    Revert "Merge pull request #8 from EarthOddity/ginta"
    This reverts commit f94196218d7adf0c469e72add8e8bbddc98a887e, reversing
    changes made to 6f3947ca969e9b89ab4f6db94184a0fa552eac72.

--- StudyBuddyAPI/Models/Review.cs
+++ StudyBuddyAPI/Models/Review.cs
@@ -1,15 +1,17 @@
-namespace StudyBuddyAPI.Models;
-
+<<<<<<< HEAD
 public class Review
 {
 
     public string ReviewText { get; set; }
     public Student Author { get; set; }
 
-
+    
     public Review(string reviewText, Student author)
     {
         ReviewText = reviewText;
         Author = author;
     }
 }
+=======
+public class Review{}
+>>>>>>> Sergio
--- StudyBuddyAPI/Models/Student.cs
+++ StudyBuddyAPI/Models/Student.cs
@@ -9,5 +9,4 @@ public class Student : User {
         //this.bachelor = bachelor;
     }
 
-    }
 }
