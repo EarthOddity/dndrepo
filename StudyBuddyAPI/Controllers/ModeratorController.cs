6468a31f46590a4ddf637017a1bde69a877e6ffb
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-10-31 16:02:46 +0100

    Merge adding student and moderator classes and controllers

---
+++ StudyBuddyAPI/Controllers/ModeratorController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using StudyBuddyAPI.models;
+using System.Collections.Generic;
+namespace StudyBuddyAPI;
+[Route("api/[controller]")]
+[ApiController]
+public class ModeratorController: ControllerBase{
+    
+    private readonly ModeratorService _moderatorService;
+
+    public ModeratorController(ModeratorService moderatorService){
+        _moderatorService = moderatorService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Moderator>>> GetModerators(){
+        var moderators = await _moderatorService.GetAllModerators();
+        return Ok(moderators);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Moderator>> GetModerator(int id){
+        var moderator = await _moderatorService.GetModeratorById(id);
+        if(moderator == null){
+            return NotFound();
+        }
+        return Ok(moderator);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Moderator>> AddModerator(Moderator moderator){
+        await _moderatorService.AddModerator(moderator);
+        return CreatedAtAction(nameof(GetModerator), new {id = moderator.id}, moderator);
+    }
+
+    /*[HttpPost("{moderatorId}/ban-review/{reviewId}")]
+    public async Task<ActionResult> BanReview(int id, Review review){
+        await _moderatorService.BanReview(id, review);
+        return NoContent();
+    }*/
+
+    [HttpPut("{id}/assignSection")]
+    public async Task<ActionResult> AssignSection(int id, [FromBody] string section){
+        await _moderatorService.AssignSection(id, section);
+        return NoContent();
+    }
+
+}
