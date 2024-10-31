bfcf468c241f7a174d6a6b8cc84e9b7a4d21097f
M316354 <316354@viauc.dk>
2024-10-31 17:31:58 +0100

    Deleting an unnecessary file

--- StudyBuddyAPI/Models/TeachingMaterial.cs
+++ StudyBuddyAPI/Models/TeachingMaterial.cs
@@ -1,4 +1,11 @@
 public class TeachingMaterial
 {
-   
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    public TeachingMaterial(string title, string description)
+    {
+        Title = title;
+        Description = description;
+    }
 }
