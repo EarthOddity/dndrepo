834f68f4897997040edd21823b72a379f980f44d
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-10-31 17:05:54 +0100

    Refactor Student model to include Bachelor property and update assembly version

--- StudyBuddyAPI/Models/Student.cs
+++ StudyBuddyAPI/Models/Student.cs
@@ -1,7 +1,7 @@
 public class Student : User {
     public bool isTutor { get; set; }
     public string language { get; set; }
-    //public Bachelor bachelor { get; set; }
+    public Bachelor bachelor { get; set; }
 
     public Student(int id, string name, string surname, string email, int phoneNumber, bool isTutor, string language ): base(id, name, surname, email, phoneNumber){
         this.isTutor = isTutor;
@@ -9,4 +9,5 @@ public class Student : User {
         //this.bachelor = bachelor;
     }
     //Hola
+
 }
