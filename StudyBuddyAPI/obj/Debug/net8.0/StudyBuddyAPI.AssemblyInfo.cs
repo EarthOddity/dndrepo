b8a78e302629bb13cba48ee330e8342176fe110c
M316354 <316354@viauc.dk>
2024-11-15 14:03:03 +0100

    commit

--- StudyBuddyAPI/obj/Debug/net8.0/StudyBuddyAPI.AssemblyInfo.cs
+++ StudyBuddyAPI/obj/Debug/net8.0/StudyBuddyAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyBuddyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b00bb27c4f744e9a052b549da5372393d3b74b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a7bea2b691f0ae1980b26f3456adaceab15b1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyBuddyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyBuddyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
