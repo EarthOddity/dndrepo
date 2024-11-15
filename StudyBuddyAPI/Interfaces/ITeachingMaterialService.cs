d220607425aa28762d88ca82921db0eed7887f18
M316354 <316354@viauc.dk>
2024-11-15 13:02:27 +0100

    update to ITMS

--- StudyBuddyAPI/Interfaces/ITeachingMaterialService.cs
+++ StudyBuddyAPI/Interfaces/ITeachingMaterialService.cs
@@ -1,3 +1,8 @@
 public interface {
     Task<IEnumerable<TeachingMaterial>> GetAllMaterials();
-}
+    Task<TeachingMaterial> GetMaterialById(int id);
+    Task<TeachingMaterial> CreateTeachingMaterial (TeachingMaterial material);
+    Task UpdateMaterial(int id, string newDescription, bool newIsApproved, Student newAuthor);
+    Task DeleteMaterial(int id);
+    Task<IEnumerable<TeachingMaterial>> GetMaterialByTitle(string title);
+    }
