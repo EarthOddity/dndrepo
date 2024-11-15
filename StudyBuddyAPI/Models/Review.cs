fd39e53340ecb234167241ec86bb5f61330908e0
M316354 <316354@viauc.dk>
2024-11-15 11:11:36 +0100

    changes to review model, service and controller

--- StudyBuddyAPI/Models/Review.cs
+++ StudyBuddyAPI/Models/Review.cs
@@ -2,18 +2,16 @@ public class Review
 {
     public int id { get; set; }
     public string reviewText { get; set; }
-    public Student author { get; set; }
-
+    public int authorId { get; set; }
+    public Student author { get; set; } 
     public bool isApproved { get; set; }
 
-    
     public Review(int id, string reviewText, Student author, bool isApproved)
     {
         this.id = id;
         this.reviewText = reviewText;
         this.author = author;
+        this.authorId = author.id; 
         this.isApproved = isApproved;
     }
-
-    
 }
