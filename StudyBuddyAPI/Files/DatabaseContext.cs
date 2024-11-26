4c215feca44e68857ccd41d62fcad37b9c322a01
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-11-26 23:07:53 +0100

    Add Entity Framework Core support and create DatabaseContext for SQLite

---
+++ StudyBuddyAPI/Files/DatabaseContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+public class DatabaseContext: DbContext 
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=studybuddy.db");
+    }
+}
