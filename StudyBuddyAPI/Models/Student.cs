c35611e64861ebe32bca6e5c1903ffa443d6eb1d
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-11-26 15:32:35 +0100

    Uncomment Bachelor property in Student model

--- StudyBuddyAPI/Models/Student.cs
+++ StudyBuddyAPI/Models/Student.cs
@@ -2,7 +2,7 @@
 public class Student : User {
     public bool isTutor { get; set; }
     public string language { get; set; }
-    //public Bachelor bachelor { get; set; }
+    public Bachelor bachelor { get; set; }
 
     public Student(int id, string name, string surname, string email, int phoneNumber, bool isTutor, string language /*Bachelor bachelor*/, string password): base(id, name, surname, email, phoneNumber, password){
         this.isTutor = isTutor;
