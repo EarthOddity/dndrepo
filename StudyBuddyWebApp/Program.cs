a598217b6b40cb11a8707cbf7c7f993c8c1efc14
Bautist1000 <91956082+Bautist1000@users.noreply.github.com>
2024-12-02 15:09:02 +0100

    Refactor services and components: replace ITeachingMaterialService and IBachelorService with IAcademicService across multiple components for improved service abstraction and maintainability

--- StudyBuddyWebApp/Program.cs
+++ StudyBuddyWebApp/Program.cs
@@ -1,6 +1,8 @@
 using StudyBuddyWebApp.Components;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,13 +11,14 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5044/") });
 
+
+
 // Register the app's services
-builder.Services.AddScoped<ITeachingMaterialService, TeachingMaterialService>();
-builder.Services.AddScoped<IBachelorService, BachelorService>();
+builder.Services.AddScoped<IAcademicService, AcademicService>();
 builder.Services.AddScoped<ISubjectService, SubjectService>();
 builder.Services.AddScoped<IReviewService, ReviewService>();
 builder.Services.AddScoped<IProfileService, ProfileService>();
-builder.Services.AddScoped<FileContext>(); 
+builder.Services.AddScoped<FileContext>();
 builder.Services.AddAuthentication().AddCookie(options =>
 {
     options.LoginPath = "/login";
