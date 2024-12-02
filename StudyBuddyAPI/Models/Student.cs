fccddd54390eaeebd354bc3205cfca6df10b9769
EarthOddity <316353@via.dk>
2024-12-02 03:13:24 +0100

    main layout, navbar, home, calendar and event pages and styling

--- StudyBuddyAPI/Models/Student.cs
+++ StudyBuddyAPI/Models/Student.cs
@@ -3,13 +3,17 @@ public class Student : User
 {
     public bool isTutor { get; set; }
     public string language { get; set; }
-    //public Bachelor bachelor { get; set; }
+    public Bachelor bachelor { get; set; }
 
     public Student(int id, string name, string surname, string email, int phoneNumber, string password, bool isTutor, string language, Bachelor bachelor) : base(id, name, surname, email, phoneNumber, password)
     {
         this.isTutor = isTutor;
         this.language = language;
         this.bachelor = bachelor;
+    }
+
+    public Student()
+    {
     }
 
 }
